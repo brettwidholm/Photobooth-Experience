569c410ad0342ddb45658e12252fbd086959b365
William Holmes <holmeswj@g.cofc.edu>
2025-03-18 20:49:01 -0400

    William test (#6)
    * Added DevMode Screen and logic to open it in DevSequence.cs
    
    * minor ui and script tidying
    
    * add .meta to .gitignore
    
    * project settings no reload
    
    * Update .gitignore with *.vscode/
    
    * Update .gitignore for .vscode/
    
    * removed .meta and .vscode/ from cache to stop tracking
    
    * Stopped tracking .vscode

--- _PEmainFile/Assets/Scripts/ScreenControl.cs
+++ _PEmainFile/Assets/Scripts/ScreenControl.cs
@@ -3,6 +3,7 @@ using TMPro;
 using UnityEngine.UI;
 public class ScreenControl : MonoBehaviour
 {
+    public GameObject devMode; //dev mode 
     public GameObject screen0; //Start Screen
     public GameObject screen1; //Instructions Screen
     public GameObject screen2; //Tap to begin screen (placeholder)
@@ -17,28 +18,55 @@ public class ScreenControl : MonoBehaviour
 
     void Start()
     {
-        Showscreen0(); //starts only showing screen0
+        //starts only showing screen0 but does not call showScreen0() to avoid fade transition on start
+        devMode.SetActive(false);
+        screen0.SetActive(true); //***
+        screen1.SetActive(false);
+        screen2.SetActive(false);
+        screen3.SetActive(false);
+        screen4.SetActive(false);
+        screen5.SetActive(false);
+        screen6.SetActive(false);
+        screen7.SetActive(false);
     }
 
     void Update()
     {
-    if(IsScreenActive("Start Screen")){
-        resetButton.gameObject.SetActive(false);
-     }
-     else{
-        resetButton.gameObject.SetActive(true);
-     }
+        if(IsScreenActive("Start Screen")){
+            resetButton.gameObject.SetActive(false);
+        }
+        else{
+            resetButton.gameObject.SetActive(true);
+        }
 
-     if(IsScreenActive("Tap to Begin Screen")){
-            if (Input.GetMouseButtonDown(0)){
-                Debug.Log("we going to the next screen");
-                ShowScreen3(); //go to photo capture screen
-            }
-     }
+        if(IsScreenActive("Tap to Begin Screen")){
+                if (Input.GetMouseButtonDown(0)){
+                    Debug.Log("we going to the next screen");
+                    ShowScreen3(); //go to photo capture screen
+                }
+        }
+    }
+
+    //I will put these in a list or an array to minimize this abomination as soon as I remember to
+    //will also change all the names to be less dumb
+    public void ShowDevMode(){//Dev Mode
+        transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(true);
+            screen0.SetActive(false);
+            screen1.SetActive(false);
+            screen2.SetActive(false);
+            screen3.SetActive(false);
+            screen4.SetActive(false);
+            screen5.SetActive(false);
+            screen6.SetActive(false);
+            screen7.SetActive(false);
+        });
+        Debug.Log("Dev mode is active!");
     }
 
     public void Showscreen0(){//Start Screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(true);
             screen1.SetActive(false);
             screen2.SetActive(false);
@@ -53,6 +81,7 @@ public class ScreenControl : MonoBehaviour
 
     public void Showscreen1(){//Instructions Screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(true);
             screen2.SetActive(false);
@@ -67,6 +96,7 @@ public class ScreenControl : MonoBehaviour
 
     public void ShowScreen2(){//Tap to begin screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(false);
             screen2.SetActive(true);
@@ -81,6 +111,7 @@ public class ScreenControl : MonoBehaviour
 
     public void ShowScreen3(){//Photo Capture
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(false);
             screen2.SetActive(false);
@@ -96,6 +127,7 @@ public class ScreenControl : MonoBehaviour
 
     public void ShowScreen4(){//Preview GIF Screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(false);
             screen2.SetActive(false);
@@ -110,6 +142,7 @@ public class ScreenControl : MonoBehaviour
 
     public void ShowScreen5(){//Info Screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(false);
             screen2.SetActive(false);
@@ -124,6 +157,7 @@ public class ScreenControl : MonoBehaviour
 
     public void ShowScreen6(){//Confirmation Screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(false);
             screen2.SetActive(false);
@@ -137,6 +171,7 @@ public class ScreenControl : MonoBehaviour
     }
         public void ShowScreen7(){//Success Screen
         transitionOverlay.FadeTransition(() => {
+            devMode.SetActive(false);
             screen0.SetActive(false);
             screen1.SetActive(false);
             screen2.SetActive(false);
