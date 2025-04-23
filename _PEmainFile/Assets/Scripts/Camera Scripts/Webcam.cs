bc6592de554af9a1332032f0fdd788b0260c0093
William Holmes <holmeswj@g.cofc.edu>
2025-04-23 14:46:49 -0400

    reupdate timer

--- _PEmainFile/Assets/Scripts/Camera Scripts/Webcam.cs
+++ _PEmainFile/Assets/Scripts/Camera Scripts/Webcam.cs
@@ -19,7 +19,7 @@ public class Webcam : MonoBehaviour
 
 
     public int state = 0;
-    public float programTime = 5.0f;
+    public float programTime = 3.5f;
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI messageText;
     public ScreenControl screenControl; 
@@ -93,7 +93,7 @@ public class Webcam : MonoBehaviour
         if(programTime <= 0.0f){
             timerText.enabled = false;
             state++;
-            programTime = 5.0f;
+            programTime = 3.5f;
         }
     }
 
@@ -114,7 +114,7 @@ public class Webcam : MonoBehaviour
         if(programTime <= 0.0f){
             timerText.enabled = false;
             state++;
-            programTime = 5.0f;
+            programTime = 3.5f;
         }
     }
 
@@ -135,7 +135,7 @@ public class Webcam : MonoBehaviour
         if(programTime <= 0.0f){
             timerText.enabled = false;
             state++;
-            programTime = 5.0f;
+            programTime = 3.5f;
         }
     }
 
@@ -155,18 +155,18 @@ public class Webcam : MonoBehaviour
         if(programTime <= 0){
             timerText.enabled = false;
             state++;
-            programTime = 5.0f;
+            programTime = 3.5f;
         }
     }
     public void Update()
     {
         if(!(screenControl.IsScreenActive("Photo Capture")) ){
             messageText.enabled = false;
-            programTime = 5.0f;
+            programTime = 3.5f;
         }
         else{
             messageText.enabled = true;
-            timerText.text = $"{programTime:F0}";
+            timerText.text = Mathf.CeilToInt(programTime).ToString();
             programTime -= Time.deltaTime;
             
             UpdateScreenReference();
